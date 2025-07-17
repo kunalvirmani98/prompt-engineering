9571572a6b7549121890008fb6cc18a137c82e1e
kunal <kunal.virmani@rategain.com>
2025-07-17 16:31:03 +0530

    port changes

--- app.py
+++ app.py
@@ -221,4 +221,5 @@ def upload_html():
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port) 
